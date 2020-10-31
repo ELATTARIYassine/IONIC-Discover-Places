88183aa41278da0d1eacc448e4b91ce23ee111c8
ELATTARIYassine <yassineattari64@gmail.com>
2020-10-31 16:28:33 +0000

    feat: added ham menu & fake data & building the pages

--- src/app/places/offers/offer-bookings/offer-bookings.page.ts
+++ src/app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { PlacesService } from "./../../places.service";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { NavController } from "@ionic/angular";
+import { Place } from "../../place.model";
 
 @Component({
-  selector: 'app-offer-bookings',
-  templateUrl: './offer-bookings.page.html',
-  styleUrls: ['./offer-bookings.page.scss'],
+  selector: "app-offer-bookings",
+  templateUrl: "./offer-bookings.page.html",
+  styleUrls: ["./offer-bookings.page.scss"],
 })
 export class OfferBookingsPage implements OnInit {
-
-  constructor() { }
+  place: Place;
+  constructor(
+    private navCtrl: NavController,
+    private route: ActivatedRoute,
+    private placesService: PlacesService
+  ) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has("placeId")) {
+        this.navCtrl.navigateBack("/places/tabs/offers");
+        return;
+      }
+      this.place = this.placesService.getPlace(paramMap.get("placeId"));
+    });
   }
-
 }
